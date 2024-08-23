848cd87218fd61e8cb6e6c0c947634e4ac53b250
dsaid <joen_tan_from.tp@tech.gov.sg>
2024-08-23 18:08:23 +0800

    refactor: removed secrets

--- frontend/src/firebase/base.js
+++ frontend/src/firebase/base.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPWDM53zQi5HvCZiurfV3PlL2N_VrYFAU",
-    authDomain: "foodbanksg-744e7.firebaseapp.com",
-    projectId: "foodbanksg-744e7",
-    storageBucket: "foodbanksg-744e7.appspot.com",
-    messagingSenderId: "666771643504",
-    appId: "1:666771643504:web:5e72e235f8939799bca8b5",
-    measurementId: "G-L9W0VY0VRS"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGER_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
