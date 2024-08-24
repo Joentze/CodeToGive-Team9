370c9e4a9b03866771e68e0542804e7662b462dc
Lucas Tay <tayboonkai@gmail.com>
2024-08-24 12:15:36 +0800

    install mui + test button

---
+++ frontend/src/pages/Onboarding.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Button from '@mui/material/Button';
+
+function Onboarding() {
+
+    return <Button variant="contained">Test</Button>;
+}
+
+export default Onboarding;
